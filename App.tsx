fd40d0b5eaf31a10bd66455b8332f042df079a68
Kurichi <yuuyapp080@gmail.com>
2023-01-10 21:40:38 +0900

    [update] 認証しているとルーティング先を変更
    auth.currentUserがnullのとき，ログイン画面に遷移するように変更

--- App.tsx
+++ App.tsx
@@ -1,11 +1,17 @@
+import { getAuth } from 'firebase/auth';
+import { View } from 'react-native';
 import { Provider } from 'react-redux';
 import { store } from './src/app/store';
-import Router from './src/components/Navigator';
+import { Router, SignIn } from './src/components/Navigator';
 
 export default function App() {
+  const auth = getAuth();
+
   return (
     <Provider store={store}>
-      <Router />
+      {auth.currentUser !== null
+        ? <Router /> : <SignIn />
+      }
     </Provider>
   );
 }
