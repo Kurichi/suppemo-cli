5ff6320f9cd77720fb18a7570704da7bb92e74fc
tami0304 <hiyokotaro.1101@gmail.com>
2023-01-14 00:53:44 +0900

    [clear] ファイル分割
    型定義ファイルが1ファイルになっていたため用途ごとのファイルに分割

---
+++ src/@types/navigation.d.ts
@@ -0,0 +1,22 @@
+
+interface NavigationProps {
+  Home: {
+    init_WS_index: number = 0;
+  };
+  TemplateList: {};
+  ChatSelector: {};
+  Camera: {};
+
+  Login: {};
+  SignUp: {};
+  Chat: { _id: string };
+
+  reader: {};
+  show: {
+    uri: string;
+    height: number;
+    width: number;
+  };
+  apply: {};
+  list: {};
+};
---
+++ src/@types/screen.d.ts
@@ -0,0 +1,22 @@
+// Screen props
+
+interface ws_props {
+  name: string;
+  card_ids: number[];
+  exists: boolean;
+}
+
+interface folder_interface {
+  id: number;
+  name: string;
+  iconName: string;
+  interface: string;
+  background_color: string;
+  cards_ids: number[];
+}
+
+interface captureImage {
+  uri: string = '';
+  height: number = 168;
+  width: number = 240;
+}
