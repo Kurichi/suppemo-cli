dc5412989003e81ea38d37b9a81a48f3dcadf8d4
Kurichi <yuuyapp080@gmail.com>
2023-01-13 14:13:47 +0900

    [add] card redux
    カードの管理用のredux

---
+++ src/@types/cards.d.ts
@@ -0,0 +1,8 @@
+interface Card {
+  id: number = -1,
+  name: string = '',
+  uri: string = '',
+  count: number = 0,
+  createdDate: Date = null,
+  isDefault: boolean = false,
+}
---
+++ src/utils/filesystem.ts
@@ -0,0 +1,11 @@
+import * as FileSystem from "expo-file-system";
+
+export const writeAsStringAsync = async (
+  fileDir: string,
+  fileName: string,
+  data: string
+) => {
+  if (!(await FileSystem.getInfoAsync(fileDir)).exists)
+    await FileSystem.makeDirectoryAsync(fileDir);
+  await FileSystem.writeAsStringAsync(fileDir + fileName, data);
+};
